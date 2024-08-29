e1fe331b97714b9d58828a2db59f3ae1a9e7e80c
A <A>
2024-08-29 14:37:40 -0700

    Extract DatabaseIndicator into it's own class

---
+++ src/nodes/DatabaseIndicator.tsx
@@ -0,0 +1,24 @@
+import { DatabaseProvider} from './types';
+import { BsHddNetwork } from 'react-icons/bs';
+import { FaGoogleDrive } from 'react-icons/fa';
+import { SiAmazonredshift } from 'react-icons/si';
+
+export function DatabaseIcon(type: DatabaseProvider): JSX.Element {
+  // Render different icon based on the type of sink node
+  switch (type) {
+    case DatabaseProvider.Fileshare:
+      return <BsHddNetwork color="grey" />;
+    case DatabaseProvider.GoogleDrive:
+      return <FaGoogleDrive color="grey" />;
+    case DatabaseProvider.RDS:
+      return <SiAmazonredshift color="grey" />;
+  }
+}
+
+export function DatabaseIndicator({ databaseProvider }: { databaseProvider: DatabaseProvider }): JSX.Element {
+  return (
+    <div style={{ padding: '10px 10px', lineHeight: '0.7px', borderRadius: '24px', border: '1px solid #e7e7e7', boxShadow: 'rgba(0, 0, 0, 0.3) 0px 2px 4px' }}>
+      {DatabaseIcon(databaseProvider)}
+    </div>
+  );
+}
--- src/nodes/SinkNode.tsx
+++ src/nodes/SinkNode.tsx
@@ -1,31 +1,14 @@
 import { memo } from 'react';
 import { Handle, Position, type NodeProps } from '@xyflow/react';
-import { type SinkNode, DatabaseProvider} from './types';
-import { BsHddNetwork } from 'react-icons/bs';
-import { FaGoogleDrive } from 'react-icons/fa';
-import { SiAmazonredshift } from 'react-icons/si';
-
-export function DatabaseIndicator(type: DatabaseProvider): JSX.Element {
-  // Render different icon based on the type of sink node
-  switch (type) {
-    case DatabaseProvider.Fileshare:
-      return <BsHddNetwork color="grey" />;
-    case DatabaseProvider.GoogleDrive:
-      return <FaGoogleDrive color="grey" />;
-    case DatabaseProvider.RDS:
-      return <SiAmazonredshift color="grey" />;
-  }
-}
+import { type SinkNode} from './types';
+import { DatabaseIndicator } from './DatabaseIndicator';
 
 export function SinkNode({ data }: NodeProps<SinkNode>): JSX.Element {
   return (
     <>
-      <div style={{ padding: '10px 10px', lineHeight: '0.7px', borderRadius: '24px', border: '1px solid #e7e7e7', boxShadow: 'rgba(0, 0, 0, 0.3) 0px 2px 4px' }}>
-        {DatabaseIndicator(data.databaseProvider)}
-      </div>
- 
-      <Handle type="target" position={Position.Left} style = {{borderColor: '#0000', background: '#0000'}}/>
-      {/* <Handle type="source" position={Position.Right} style = {{background: 'white', border: 'solid #53B177'}}/> */}
+      <DatabaseIndicator databaseProvider={data.databaseProvider} />
+      <Handle type="target" position={Position.Left} style={{ borderColor: '#0000', background: '#0000' }} />
+      {/* <Handle type="source" position={Position.Right} style={{ background: 'white', border: 'solid #53B177' }} /> */}
     </>
   );
 }
