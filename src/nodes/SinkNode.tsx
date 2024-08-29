b2c4513c91a6197ebeea83390b17727738a8638e
A <A>
2024-08-29 14:58:14 -0700

    Extract Source and Target Handle

--- src/nodes/SinkNode.tsx
+++ src/nodes/SinkNode.tsx
@@ -1,14 +1,14 @@
 import { memo } from 'react';
-import { Handle, Position, type NodeProps } from '@xyflow/react';
+import { type NodeProps } from '@xyflow/react';
 import { type SinkNode} from './types';
 import { DatabaseIndicator } from './DatabaseIndicator';
+import TargetHandle from './TargetHandle';
 
 export function SinkNode({ data }: NodeProps<SinkNode>): JSX.Element {
   return (
     <>
       <DatabaseIndicator databaseProvider={data.databaseProvider} />
-      <Handle type="target" position={Position.Left} style={{ borderColor: '#0000', background: '#0000' }} />
-      {/* <Handle type="source" position={Position.Right} style={{ background: 'white', border: 'solid #53B177' }} /> */}
+      <TargetHandle />
     </>
   );
 }
