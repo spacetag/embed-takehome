8b03a95e955073ae561dba3c084eb37f8070669f
A <A>
2024-08-29 16:12:04 -0700

    Add DatabaseProvider to SoruceDatasetNode

--- src/nodes/SourceDatasetNode.tsx
+++ src/nodes/SourceDatasetNode.tsx
@@ -4,10 +4,14 @@ import { type SourceDatasetNode} from './types';
 import { PiStackFill } from 'react-icons/pi';
 import SourceHandle from './SourceHandle';
 import TargetHandle from './TargetHandle';
+import { DatabaseIndicator } from './DatabaseIndicator';
 
 export function SourceDatasetNode({ data }: NodeProps<SourceDatasetNode>): JSX.Element {
   return (
     <div style={{ height: '34px', width: '34px'}}>
+      <div style={{color: '#323333', fontSize: '12px', padding: '0px 8px', marginTop: '6px', position: 'absolute', top: '-3px', left: '-54px'}}>
+        <DatabaseIndicator databaseProvider={data.databaseProvider} />
+      </div>
       <div style={{ padding: '8px 8px', lineHeight: 0.7, border: '1px solid #BBBBBB', borderRadius: '8px', background: 'white', width: '16px' }}>
         <PiStackFill style={{ color: '#30BBF7' }} />
       </div>
