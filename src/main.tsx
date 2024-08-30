215b988d0495baf71062b8e30e00a4bc541dddd0
A <A>
2024-08-30 00:24:14 -0700

    Change App to PipelineGraph

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import PipelineGraph from './PipelineGraph';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PipelineGraph />
   </React.StrictMode>
 );
