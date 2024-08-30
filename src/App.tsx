5c6dbd817d9b86d1707ded814cd61c2d9680669b
A <A>
2024-08-30 09:51:07 -0700

    Update Layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,10 @@ import PipelineGraph from "./PipelineGraph";
 
 export default function App() {
   return (
-    <div style={{width: '100%', height: '100%'}}>
-      <div>
-        <div style={{ color: 'white', fontSize: '24px', padding: '16px', background: '#3C3F4A' }}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <IoHomeOutline style={{ color: 'white', marginRight: '10px' }} />
-            <div>Pipeline</div>
-          </div>
-        </div>
+    <div style={{width: '100%', height: '100%', display: 'flex', flexDirection: 'column'}}>
+      <div style={{ display: 'flex', alignItems: 'center', color: 'white', fontSize: '24px', padding: '16px', background: '#3C3F4A' }}>
+        <IoHomeOutline style={{ color: 'white', marginRight: '10px' }} />
+        <div>Pipeline</div>
       </div>
       <PipelineGraph />
     </div>
